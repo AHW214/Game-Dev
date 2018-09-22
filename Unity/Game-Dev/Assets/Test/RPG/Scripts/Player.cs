14516adab8ba18418760f4f898713801ab9580c4
Adam <adamweider98@gmail.com>
2018-09-21 20:25:01 -0500

    Roll animation tests [big code redundancy gotta clean that up (maybe we should use some animator state transitions)]

--- Unity/Game-Dev/Assets/Test/RPG/Scripts/Player.cs
+++ Unity/Game-Dev/Assets/Test/RPG/Scripts/Player.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 namespace Game
 {
     public class Player : MovingObject
-    {
+    {     
         private Animator ar;
+        private Coroutine inst = null;
 
         private new void Start()
         {
             base.Start();
             ar = GetComponent<Animator>();
+            ar.Play("Idle");
         }
 
         private void Update()
@@ -23,7 +25,7 @@ namespace Game
         {
             Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-            if (input.magnitude >= 0.5F)
+            if (input.magnitude >= 0.1F)
             {
                 heading = new Direction(input);
 
@@ -31,9 +33,55 @@ namespace Game
                 Debug.DrawRay(pos, input.normalized, Color.red, Time.deltaTime);
                 Debug.DrawRay(pos, heading.v2, Color.blue, Time.deltaTime);
 
-                Move();
+                ar.SetFloat("FaceX", heading.v2.x);
+                ar.SetFloat("FaceY", heading.v2.y);
+
+                if (input.magnitude >= 0.7F)
+                {
+                    if (Input.GetButton("B"))
+                    {
+                        ar.Play("Roll");
+                        movementSpeed = 1.5F;
+                    }
+
+                    else
+                    {
+                        ar.Play("Walk");
+                        movementSpeed = 1.0F;
+                    }
+                    
+                    Move();
+                }
+
+                else if (!ar.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+                {
+                    ar.Play("Idle");
+                }
             }
+
+            else if (!ar.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+            {
+                ar.Play("Idle");
+            }
+        }
+
+        private IEnumerator OneTimeAnim(string stateName)
+        {
+            ar.Play(stateName);
+
+            while (!ar.GetCurrentAnimatorStateInfo(0).IsName(stateName))
+            {
+                yield return null;
+            }
+
+            float length = ar.GetCurrentAnimatorStateInfo(0).length;
+
+            for (float t = 0.0F; t < length; t += Time.deltaTime)
+            {
+                yield return null;
+            }
+
+            inst = null;
         }
     }
 }
-
