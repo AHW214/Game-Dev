2953a4aa563476a3caebab08cc779cc0a8b76179
Adam <adamweider98@gmail.com>
2018-09-24 21:46:34 -0500

    Jump mechanics [following a tutorial and need to refactor big time].

--- Unity/Game-Dev/Assets/Test/Platformer/Scripts/Controller2D.cs
+++ Unity/Game-Dev/Assets/Test/Platformer/Scripts/Controller2D.cs
@@ -6,11 +6,13 @@ namespace Platformer
 {
     [RequireComponent(typeof(BoxCollider2D))]
     public class Controller2D : MonoBehaviour
-    {     
+    {
+        public LayerMask collisionMask;
         public int horizontalRayCount = 4;
         public int verticalRayCount = 4;
-
         public bool drawRaycasts = false;
+
+        public CollisionInfo collisions;
 
         private const float skinWidth = 0.015F;
 
@@ -23,16 +25,86 @@ namespace Platformer
         private void Start()
         {
             collider = GetComponent<BoxCollider2D>();
+
+            CalculateRaySpacing();
         }
 
         private void Update()
         {
-            UpdateRaycastOrigins();
-            CalculateRaySpacing();
 
-            if (drawRaycasts)
+        }
+
+        public void Move(Vector2 displacement)
+        {
+            UpdateRaycastOrigins();
+            collisions.Reset();
+
+            if (displacement.x != 0)
             {
-                DrawRaycasts();
+                HorizontalCollisions(ref displacement);
+            }
+
+            if (displacement.y != 0)
+            {
+                VerticalCollisions(ref displacement);
+            }
+
+            transform.Translate(displacement);
+        }
+
+        private void HorizontalCollisions(ref Vector2 displacement)
+        {
+            float directionX = Mathf.Sign(displacement.x);
+            float rayLength = Mathf.Abs(displacement.x) + skinWidth;
+
+            Vector2 rayOrigin = directionX == -1 ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+
+            for (int i = 0; i < horizontalRayCount; i++)
+            {
+                Vector2 offsetOrigin = rayOrigin + Vector2.up * (i * horizontalRaySpacing);
+                RaycastHit2D hit = Physics2D.Raycast(offsetOrigin, directionX * Vector2.right, rayLength, collisionMask);
+
+                if (hit)
+                {
+                    displacement.x = directionX * (hit.distance - skinWidth);
+                    rayLength = hit.distance;
+
+                    collisions.left = directionX == -1;
+                    collisions.right = directionX == 1;
+                }
+
+                if (drawRaycasts)
+                {
+                    Debug.DrawRay(offsetOrigin, rayLength * directionX * Vector2.right, Color.red);
+                }
+            }
+        }
+
+        private void VerticalCollisions(ref Vector2 displacement)
+        {
+            float directionY = Mathf.Sign(displacement.y);
+            float rayLength = Mathf.Abs(displacement.y) + skinWidth;
+
+            Vector2 rayOrigin = directionY == -1 ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+
+            for (int i = 0; i < verticalRayCount; i++)
+            {
+                Vector2 offsetOrigin = rayOrigin + Vector2.right * (i * verticalRaySpacing + displacement.x);
+                RaycastHit2D hit = Physics2D.Raycast(offsetOrigin, directionY * Vector2.up, rayLength, collisionMask);
+
+                if (hit)
+                {
+                    displacement.y = directionY * (hit.distance - skinWidth);
+                    rayLength = hit.distance;
+
+                    collisions.below = directionY == -1;
+                    collisions.above = directionY == 1;
+                }
+
+                if (drawRaycasts)
+                {
+                    Debug.DrawRay(offsetOrigin, rayLength * directionY * Vector2.up, Color.red);
+                }
             }
         }
 
@@ -65,26 +137,22 @@ namespace Platformer
             verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
         }
 
-        private void DrawRaycasts()
-        {
-            Vector2 horizontalOffset = horizontalRaySpacing * Vector2.down;
-            Vector2 verticalOffset = verticalRaySpacing * Vector2.right;
-
-            for (int i = 0; i < horizontalRayCount; i++)
-            {
-                Debug.DrawRay(raycastOrigins.topRight + i * horizontalOffset, 2 * Vector2.right, Color.red);
-            }
-
-            for (int i = 0; i < verticalRayCount; i++)
-            {
-                Debug.DrawRay(raycastOrigins.bottomLeft + i * verticalOffset, 2 * Vector2.down, Color.red);
-            }
-        }
-
-        struct RaycastOrigins
+        private struct RaycastOrigins
         {
             public Vector2 topLeft, topRight;
             public Vector2 bottomLeft, bottomRight;
+        }
+
+        public struct CollisionInfo
+        {
+            public bool above, below;
+            public bool left, right;
+
+            public void Reset()
+            {
+                above = below = false;
+                left = right = false;
+            }
         }
     }
 }
--- Unity/Game-Dev/Assets/Test/Platformer/Scripts/Player.cs
+++ Unity/Game-Dev/Assets/Test/Platformer/Scripts/Player.cs
@@ -7,16 +7,51 @@ namespace Platformer
     [RequireComponent(typeof(Controller2D))]
     public class Player : MonoBehaviour
     {
-        Controller2D controller;
+        public float jumpHeight = 4.0F;
+        public float timeToJumpApex = 0.4F;
+        public float accelerationTimeAirborne = 0.2F;
+        public float accelerationTimeGrounded = 0.1F;
+        public float movementSpeed = 6.0F;
+
+        private float gravity;
+        private float jumpSpeed;
+        private float velocityXSmoothing;
+
+        private Vector2 velocity = Vector2.zero;
+        private Controller2D controller;
 
         private void Start()
         {
             controller = GetComponent<Controller2D>();
+
+            gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+            jumpSpeed = Mathf.Abs(gravity) * timeToJumpApex;
+
+            Debug.Log($"Gravity: {gravity}\nJump Speed: {jumpSpeed}");
         }
 
         private void Update()
         {
+            if (controller.collisions.above || controller.collisions.below)
+            {
+                velocity.y = 0;
+            }
 
+            Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
+            {
+                velocity.y = jumpSpeed;
+            }
+
+            float targetVelocityX = input.x * movementSpeed;
+
+            velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, 
+                                          controller.collisions.below ? accelerationTimeGrounded : accelerationTimeAirborne);
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
         }
     }
 }
